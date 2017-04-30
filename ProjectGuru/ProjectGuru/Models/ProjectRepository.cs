bee5da8885bedc100ae0a9ddfb0e548efe443945
simone.colucci <simone.colucci@techedgegroup.com>
2017-04-30 18:08:55 +0200

    Added ProjectRepository class

---
+++ ProjectGuru/ProjectGuru/Models/ProjectRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ProjectGuru.Models
+{
+    public class ProjectRepository : Repository<Project>
+    {
+        public ProjectRepository(DataBase db) : base(db) { }
+
+        public Project Find(string project)
+        {
+            return db.Projects.Where(p => p.Name.Equals(project)).Include(p => p.Activities).FirstOrDefault();
+        }
+
+        public void Remove(string project)
+        {
+            Project projectObject = Find(project);
+            projectObject.Activities.RemoveAll(a => true);
+            base.Remove(project);
+        }
+    }
+}
